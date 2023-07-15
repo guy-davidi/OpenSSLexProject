b9825fafd493161362a066e402696c813587e1d1
Guy Davidi <64970907+guy-davidi@users.noreply.github.com>
2023-07-15 11:36:06 +0300

    Create openssl_example.h

---
+++ openssl_example.h
@@ -0,0 +1,12 @@
+#ifndef OPENSSL_EXAMPLE_H
+#define OPENSSL_EXAMPLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+
+#define RSA_KEY_LENGTH 2048
+
+#endif /* OPENSSL_EXAMPLE_H */
